43c9daa1ca214ffe831052c601450c7a9f473527
Gerard Hynes <gkhynes@gmail.com>
2018-06-24 17:12:46 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,53 @@
+document.getElementById("button").addEventListener("click", loadData);
+
+function loadData() {
+  // Create an XHR Object
+  const xhr = new XMLHttpRequest();
+
+  // OPEN
+  xhr.open("GET", "data.txt", true);
+
+  // console.log("READYSTATE: ", xhr.readyState);
+
+  // OLDER SYNTAX
+  // xhr.onreadystatechange = function() {
+  //   console.log("READYSTATE: ", xhr.readyState);
+  //   if (this.status === 200 && this.readyState === 4) {
+  //     console.log(this.responseText);
+  //   }
+  // };
+
+  // Optional - used for spinners/loaders
+  xhr.onprogress = function() {
+    console.log("READYSTATE: ", xhr.readyState);
+  };
+
+  xhr.onload = function() {
+    console.log("READYSTATE: ", xhr.readyState);
+    // Check HTTP status
+    if (this.status === 200) {
+      console.log(this.responseText);
+      document.getElementById("output").innerHTML = `<h1>${
+        this.responseText
+      }</h1>`;
+    }
+  };
+
+  xhr.onerror = function() {
+    console.log("Request error...");
+  };
+
+  xhr.send();
+}
+
+// HTTP Statuses
+// 200: 'OK'
+// 403: 'Forbidden'
+// 404: 'Not found'
+
+// readyState Values
+// 0: request not initialized
+// 1: server connection established
+// 2: request received
+// 3: processing request
+// 4: request finished and response ready
